e9c9a8d74b93b863694f52cd9e205c337d289223
MrGoumX <goumaschristos@gmail.com>
2019-07-27 12:25:21 +0300

    Added room mapping for API & greek character conversion for AUEB

--- types/context.go
+++ types/context.go
@@ -5,10 +5,11 @@ import "github.com/labstack/echo"
 type Context struct {
 	echo.Context
 	plugin Plugin
+	Rooms  *RoomMap
 }
 
-func NewContext(context echo.Context, plugin Plugin) *Context {
-	return &Context{Context: context, plugin: plugin}
+func NewContext(context echo.Context, plugin Plugin, rooms *RoomMap) *Context {
+	return &Context{Context: context, plugin: plugin, Rooms: rooms}
 }
 
 func (c Context) Plugin() Plugin {
