0d6e1721458347581552018d4a7328db99d3add9
enderian <spyridon@ender.gr>
2019-08-10 10:52:46 +0300

    Added components

---
+++ app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <TitleBar/>
+</template>
+
+<script>
+import TitleBar from './components/TitleBar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TitleBar
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap.scss";
+</style>
---
+++ app/src/components/TitleBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="navbar navbar-expand navbar-dark">
+        <a class='navbar-brand'>
+            <img v-bind:src="'https://www.aueb.gr/press/logos/1_AUEB-pantone-HR.jpg'"
+                 alt="logo" height="44" />
+        </a>
+        <nav class="navbar-nav mr-auto">
+            <a class="navbar-brand">AUEB CSLabs</a>
+        </nav>
+        <a class="navbar-brand">
+            {{ new Date() }}
+        </a>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "TitleBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
