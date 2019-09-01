5ea79118c10f33aaf6f80280d47ea560a69a7c82
Christos Papageorgiou <root.expert.xda@gmail.com>
2019-09-01 18:33:53 +0300

    moniteur: web: Add department's logo.
    Signed-off-by: Christos Papageorgiou <root.expert.xda@gmail.com>

--- app/src/components/TitleBar.vue
+++ app/src/components/TitleBar.vue
@@ -5,7 +5,9 @@
                  alt="logo" height="44" />
         </a>
         <nav class="navbar-nav mr-auto">
-            <a class="navbar-brand">AUEB CSLabs</a>
+            <a class="navbar-brand mt-2">AUEB CSLabs</a>
+            <img class="ml-2" v-bind:src="'https://www.dept.aueb.gr/schools_department_photos/cs.png'"
+                 alt="cs_logo" height="55" />
         </nav>
         <a class="navbar-brand">
             {{date}}
