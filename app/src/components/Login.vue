9be903b072e5abde78d411db5b0c8c80f724e5c5
MrGoumX <goumaschristos@gmail.com>
2019-09-20 16:51:02 +0300

    Login changes.

--- app/src/components/Login.vue
+++ app/src/components/Login.vue
@@ -1,20 +1,18 @@
 <template>
-    <div>
+    <div style="width: 25%;">
         <p>Login to Moniteur</p>
-        <div class="row">
-            <form @submit="login">
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" class="form-control" id="username" v-model="loginForm.username" placeholder="Enter username">
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" id="password" v-model="loginForm.password" placeholder="Password">
-                </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
-            </form>
-        </div>
-        <p v-bind="login">{{error}}</p>
+        <form @submit="checkLogin">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" v-model="loginForm.username" placeholder="Enter username">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" v-model="loginForm.password" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </form>
+        <p>{{error}}</p>
     </div>
 </template>
 
@@ -36,7 +34,7 @@
         },
 
         methods: {
-            login: function (e) {
+            login: function () {
                 axios({
                     method: 'post',
                     url: this.$root.$data['api'] + '/api/authenticate',
@@ -56,6 +54,16 @@
                         this.error = "Invalid username or password.";
                     }
                 });
+            },
+
+            checkLogin: function (e) {
+                if (this.loginForm.username === '' || this.loginForm.password === '') {
+                    this.error = "You have not entered username or password!";
+                    e.preventDefault();
+                    return;
+                }
+                this.error = '';
+                this.login();
                 e.preventDefault();
             }
         }
