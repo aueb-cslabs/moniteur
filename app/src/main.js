4cd86cacc6490d840d2e996156384e5fb1b46d82
Christos Papageorgiou <root.expert.xda@gmail.com>
2019-11-13 21:58:19 +0200

    moniteur: web: Nuke Vue Router.
    Signed-off-by: Christos Papageorgiou <root.expert.xda@gmail.com>

--- app/src/main.js
+++ app/src/main.js
@@ -1,36 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueI18n from 'vue-i18n'
-import VueRouter from 'vue-router'
-
-import Moniteur from "./components/Moniteur";
-import P404 from "./components/Page404/404";
 
 import messages from './translations'
 
 Vue.config.productionTip = false;
-Vue.use(VueI18n);
-const routes = [
-  {path: '/notfound', component: P404},
-  {path: '/:id', component: Moniteur},
-  {path: '/', redirect: '/notfound'}
-];
 
-Vue.use(VueRouter);
+Vue.use(VueI18n);
 
 const i18n = new VueI18n({
   locale: 'el',
   messages,
 });
 
-const router = new VueRouter({
-  mode: 'history',
-  routes
-});
-
 new Vue({
   i18n,
-  router,
 
   render: h => h(App),
 }).$mount('#app');
