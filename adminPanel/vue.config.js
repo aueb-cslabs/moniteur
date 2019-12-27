70dc467228afab85dccaeac9d9673af99378a690
Christos Papageorgiou <root.expert.xda@gmail.com>
2019-12-27 13:31:14 +0200

    moniteur: adminPanel: Use and configure Vue-cli-electron-builder.
     * Configure to build Windows installer.
    
    Signed-off-by: Christos Papageorgiou <root.expert.xda@gmail.com>

--- adminPanel/vue.config.js
+++ adminPanel/vue.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-    publicPath : process.env.NODE_ENV === 'production' ? './' : '/'
+    publicPath : process.env.NODE_ENV === 'production' ? './' : '/',
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                // options placed here will be merged with default configuration and passed to electron-builder
+                "appId": "org.aueb.moniteur-admin",
+                "productName": "moniteur-admin",
+                "nsis": {
+                    "oneClick": false,
+                    "artifactName": "${productName}-Setup-${version}.${ext}"
+                },
+                "dmg": {
+
+                }
+
+            }
+        }
+    }
 }
