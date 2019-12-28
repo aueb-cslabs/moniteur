df84729393fa9cec9fa05491fe86db5fed75a439
Christos Papageorgiou <root.expert.xda@gmail.com>
2019-12-28 18:08:44 +0200

    moniteur: adminPanel: Set autoHideMenuBar to true.
     * Hides the menu bar. Press ALT to un-hide it.
    
    Signed-off-by: Christos Papageorgiou <root.expert.xda@gmail.com>

--- adminPanel/src/background.js
+++ adminPanel/src/background.js
@@ -15,7 +15,7 @@ protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, webPreferences: {
+  win = new BrowserWindow({ width: 800, height: 600, autoHideMenuBar: true, webPreferences: {
     nodeIntegration: true
   } })
 
