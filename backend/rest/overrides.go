b4f325d6499e7c3c79dda17250cb96ce8c0b8df8
MrGoumX <goumaschristos@gmail.com>
2019-12-29 09:17:49 +0200

    Forgot GET request for lesson overriding

--- backend/rest/overrides.go
+++ backend/rest/overrides.go
@@ -9,6 +9,7 @@ import (
 func OverrideGroup(g *echo.Group) {
 	g.POST("", Validate(createOverride))
 	g.DELETE("", Validate(deleteOverride))
+	g.GET("", Validate(overrides))
 }
 
 func createOverride(e echo.Context) error {
@@ -37,4 +38,14 @@ func deleteOverride(e echo.Context) error {
 
 	db.Delete(types.ScheduleSlot{}, "id = ?", override.Model.ID)
 	return e.NoContent(http.StatusOK)
+}
+
+func overrides(e echo.Context) error {
+	db := e.(*types.Context).DB
+
+	var overrides []*types.ScheduleSlot
+
+	db.Find(&overrides)
+
+	return e.JSON(http.StatusOK, overrides)
 }
