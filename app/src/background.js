1532722e275768a3bc7bbed184c7ad037df55a8c
Christos Papageorgiou <root.expert.xda@gmail.com>
2020-01-17 01:21:26 +0200

    moniteur: web: Make window fullscreen.
    Signed-off-by: Christos Papageorgiou <root.expert.xda@gmail.com>

--- app/src/background.js
+++ app/src/background.js
@@ -18,7 +18,7 @@ protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, frame: false, webPreferences: {
+  win = new BrowserWindow({ width: 800, height: 600, frame: false, fullscreen: true, webPreferences: {
     nodeIntegration: true
   } })
 
